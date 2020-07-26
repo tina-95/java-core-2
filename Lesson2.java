d681d78e9d78573636c3a2fe6ea49bd0b8f78a11
tina-95 <47135919+tina-95@users.noreply.github.com>
2020-07-26 14:10:02 +0300

    Add files via upload

---
+++ Lesson2.java
@@ -0,0 +1,20 @@
+public class Lesson2 {
+
+    public int method(String[][] arr) {
+        if (arr.length != 4 || arr[0].length != 4 || arr[1].length != 4 || arr[2].length != 4 || arr[3].length != 4) {
+            throw new MyArraySizeException();
+        }
+        int sum = 0;
+        for (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j, arr[i][j]);
+                }
+            }
+        }
+        return sum;
+    }
+
+}
